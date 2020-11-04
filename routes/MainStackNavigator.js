5e69ecb2ebb99453e74591bd119cc7dcdff4659b
dndnjs996 <dndnjs96@naver.com>
2020-11-04 11:12:00 +0900

    Home

--- routes/MainStackNavigator.js
+++ routes/MainStackNavigator.js
@@ -3,13 +3,21 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Layout from "../screens/layout"
+import Home from "../screens/home"
 
 const Stack = createStackNavigator();
 
 function MainStackNavigator() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName='Layout'>
+            <Stack.Navigator initialRouteName='Home'>
+                
+            <Stack.Screen 
+                    name = "Home"
+                    component = {Home}
+                    options={{ title: 'home Screen'}}
+                />
+                
                 <Stack.Screen 
                     name = "Layout"
                     component = {Layout}
